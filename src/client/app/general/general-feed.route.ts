e466c697bd70bc9b0e75ba5060f114e6aa9f890d
John Hennelly <jjhennelly@gmail.com>
2017-07-11 19:46:41 -0400

    Addcomments not adding all comments issue fixed. comment issue not being able to access bc nested in unique id with nested ng-repeat - and cleaned code a little

--- src/client/app/general/general-feed.route.ts
+++ src/client/app/general/general-feed.route.ts
@@ -16,6 +16,7 @@ namespace friendlyPix {
                         controllerAs: 'gc',
                         resolve: {
                             generalDataDescending: (friendlyFire, $q) => {
+
                                 // TODO: should this go in the method as property ie. data.reversedEntries
                                 return friendlyFire.getPosts().then((data) => {
                                     // Reverse data entries so descending by date
@@ -28,29 +29,14 @@ namespace friendlyPix {
 
                                     for (let i = p.length - 1; i >= 0; i--) {
 
-                                        // set up comments
-                                        // console.log('p[i]', p[i]);
-
+                                        // TODO: abstraction and docs
                                         reversedPostData[p[i]] = data.entries[p[i]];
                                         var promises = [];
                                         var defer = $q.defer();
-                                        // console.log(p[i], 'p[i]');
 
                                      ((j) => {
-                                        //  console.log(j, 'j');
                                         var commentData = friendlyFire.getComments(j).then((c) => {
-                                                    // console.log('c.entries', c.entries);
-                                                    // TODO: work around bc c.entries
-                                                    // had a unique id as the key for the comment that I was unable to
-                                                    // access in the ng-repeat
-                                                    // how to I clear in thei scenario the object of the unique key
-                                                    var a = Object.keys(c.entries);
-                                                    angular.forEach(a, (b) => {
-                                                        reversedPostData[p[i]]['comments'] = c.entries[b];
-                                                    });
-
-                                                    // console.log('reversedPostData', reversedPostData[p[i]]);
-
+                                                    reversedPostData[j]['comments'] = c.entries;
                                                 });
                                         promises.push(commentData);
 
@@ -60,15 +46,7 @@ namespace friendlyPix {
                                          defer.resolve(reversedPostData);
                                      });
 
-
-                                        // console.log('reversedPostData', reversedPostData);
-                                        // console.log('commentData', commentData);
-                                        // reversedPostData[p[i]] = data.entries[p[i]];
-                                        // reversedPostData[p[i]] = commentData;
                                     }
-
-
-                                    // console.log('defer p', defer.promise);
                                     return defer.promise;
                                 });
                                 //
